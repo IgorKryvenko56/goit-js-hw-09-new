f66d5916d7dd3da9f5a6a80c4c19ad2145038c08
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-04-14 22:40:00 +0300

    JS-HW-09new done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+ const startButton = document.getElementsByTagName('button')[0];
+ const stopButton = document.getElementsByTagName('button')[1];
+
+ function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+ }
+
+//  let intervalID;
+
+// startButton.addEventListener('click', startButtonFunc);
+// function startButtonFunc() {
+//   intervalID = setInterval(() => {
+//     document.body.style.backgroundColor = `${getRandomHexColor()}`;
+//   }, 1000);
+//   startButton.disabled = true;
+// }
+
+// stopButton.addEventListener('click', stopButtonFunc);
+// function stopButtonFunc() {
+//   clearInterval(intervalID);
+//   startButton.disabled = false;
+// }
+// Function of changing background colour
+		 let intervalId;
+		function changeBackgroundColor() {
+		document.body.style.backgroundColor = getRandomHexColor();
+		}
+
+		startButton.addEventListener('click', () => {
+		intervalId = setInterval(changeBackgroundColor, 1000);
+		});
+
+		stopButton.addEventListener('click', () => {
+			clearInterval(intervalId);
+		});
+	
+
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+const amountInput = document.querySelector('[name="amount"]');
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  const amount = parseInt(amountInput.value);
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  
+  // Вызываем промис
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay + (i * step))
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
+
