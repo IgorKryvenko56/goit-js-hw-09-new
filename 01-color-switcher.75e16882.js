52149d35288ff9dd9891659b7c8299c044567ce5
IgorKryvenko56 <117092561+IgorKryvenko56@users.noreply.github.com>
2023-04-16 09:01:18 +0000

    Deploying to gh-pages from @ IgorKryvenko56/goit-js-hw-09-new@fd87c044a35a85e6282236754ac2d97b7a009f50 🚀

--- 01-color-switcher.75e16882.js
+++
@@ -1,2 +0,0 @@
-const t=document.getElementsByTagName("button")[0],e=document.getElementsByTagName("button")[1];let n;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{n=setInterval(a,1e3)})),e.addEventListener("click",(()=>{clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.75e16882.js.map
