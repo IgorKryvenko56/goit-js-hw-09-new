fd87c044a35a85e6282236754ac2d97b7a009f50
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-04-16 12:00:52 +0300

    JS-HW-09new btn start disabled

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,40 +1,23 @@
- const startButton = document.getElementsByTagName('button')[0];
- const stopButton = document.getElementsByTagName('button')[1];
+ // Function of changing background color
+const startButton = document.getElementsByTagName('button')[0];
+const stopButton = document.getElementsByTagName('button')[1];
 
- function getRandomHexColor() {
-   return `#${Math.floor(Math.random() * 16777215)
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
- }
+}
+function changeBackgroundColor() {
+	document.body.style.backgroundColor = getRandomHexColor();
+  }
 
-//  let intervalID;
-
-// startButton.addEventListener('click', startButtonFunc);
-// function startButtonFunc() {
-//   intervalID = setInterval(() => {
-//     document.body.style.backgroundColor = `${getRandomHexColor()}`;
-//   }, 1000);
-//   startButton.disabled = true;
-// }
-
-// stopButton.addEventListener('click', stopButtonFunc);
-// function stopButtonFunc() {
-//   clearInterval(intervalID);
-//   startButton.disabled = false;
-// }
-// Function of changing background colour
-		 let intervalId;
-		function changeBackgroundColor() {
-		document.body.style.backgroundColor = getRandomHexColor();
-		}
-
-		startButton.addEventListener('click', () => {
-		intervalId = setInterval(changeBackgroundColor, 1000);
-		});
-
-		stopButton.addEventListener('click', () => {
-			clearInterval(intervalId);
-		});
-	
-
+ let intervalId;
+ startButton.addEventListener('click', () => {
+ startButton.disabled = true;
+ intervalId = setInterval(changeBackgroundColor, 1000);
+ });
+ stopButton.addEventListener('click', () => {
+   clearInterval(intervalId);
+  startButton.disabled = false;
+});
 
